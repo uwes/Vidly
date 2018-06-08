d3d28652610539c50f34a681cc7c9d79a09189f8
Uwe Schneider <uschneider@reliantbh.com>
2018-06-08 12:42:18 -0700

    Implemented progess up to Section 2 View Models.

--- Vidly/App_Start/RouteConfig.cs
+++ Vidly/App_Start/RouteConfig.cs
@@ -13,6 +13,17 @@ namespace Vidly
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
+            routes.MapMvcAttributeRoutes(); // set up Attribute Routing --> routing handled in the Controller
+
+            //// custom route: typically don't do it here! ... use attributes (Attribute Routing)
+            //routes.MapRoute(
+            //    "MoviesByReleaseDate",
+            //    "movies/released/{year}/{month}",
+            //    new { controller = "Movies", action = "ByReleaseDate" },
+            //    new { year = @"\d{4}", month = @"\d{2}" } // year with 4 chars, month with 2 chars (e.g.: 4  -->  04)
+            //    // new { year = @"2014|2015|2016|2017", month = @"\d{2}" }); // select from specified 4 given years, all others will be rejected
+            //);
+
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Vidly.Models;
+using Vidly.ViewModels;
 
 namespace Vidly.Controllers
 {
@@ -13,8 +14,54 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
 
-            return View(movie);
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            //ViewData["Movie"] = movie; // ViewData: each Controller has ViewData object as ViewDataDictionary
+            //ViewBag.Movie = movie; // ViewBag: Movie property is added to the ViewBag at runtime (no compiler errors)
+            //return View();
+
+            //// Samples on how to pass model data to view as argument; return a ViewResult
+            return View(viewModel); 
+            //return Content("Hello World!");
+            //return HttpNotFound();
+            //return new EmptyResult();
+            //return RedirectToAction("Index", "Home", new {  page = 1, sortBy = "name" });
+        }
+
+        [Route("movies/released/{year:regex(\\d{4}):range(1900, 2018)}/{month}")]
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("id=" + id);
+        }
+
+        // movies
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
         }
     }
 }
